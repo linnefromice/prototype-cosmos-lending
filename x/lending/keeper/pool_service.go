2b8229f0df59dabc207c62936d5f1118d7f731bb
linnefromice <linnefromice@gmail.com>
2022-11-21 20:16:44 +0900

    update: add pool - create resource account

--- x/lending/keeper/pool_service.go
+++ x/lending/keeper/pool_service.go
@@ -2,29 +2,53 @@ package keeper
 
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/address"
 	"github.com/linnefromice/lending/x/lending/types"
+
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
 )
 
+func newPairPoolAddress(poolId uint64) sdk.AccAddress {
+	key := append([]byte("pairpool"), sdk.Uint64ToBigEndian(poolId)...)
+	return address.Module(types.ModuleName, key)
+}
+
+func (k Keeper) createModuleAccount(ctx sdk.Context, addr sdk.AccAddress) error {
+	account := k.accountKeeper.NewAccount(
+		ctx,
+		authtypes.NewModuleAccount(
+			authtypes.NewBaseAccountWithAddress(addr),
+			addr.String(),
+		),
+	)
+	k.accountKeeper.SetAccount(ctx, account)
+	return nil
+}
+
 func (k Keeper) AddPool(ctx sdk.Context, msg *types.MsgAddPool) (types.PairPool, error) {
-	creator, _ := sdk.AccAddressFromBech32(msg.Creator)
+	// creator, _ := sdk.AccAddressFromBech32(msg.Creator)
 	poolId := k.GetPairPoolCount(ctx) + 1
 
 	// validations
 
+	// convert
+	poolAccAddress := newPairPoolAddress(poolId)
+	k.createModuleAccount(ctx, poolAccAddress)
+
 	pool := types.PairPool{
-		Address:                    creator.String(), // temp
+		Address:                    poolAccAddress.String(),
 		PoolId:                     poolId,
-		AssetLiquidity:             msg.Amount,            // temp
-		AssetLpCoinDenom:           msg.Amount.GetDenom(), // temp
-		AssetTotalNormalDeposited:  0,
+		AssetLiquidity:             msg.Amount, // TODO: collect
+		AssetLpCoinDenom:           msg.Amount.GetDenom(),
+		AssetTotalNormalDeposited:  0, // TODO: collect
 		AssetTotalConlyDeposited:   0,
 		AssetTotalBorrowed:         0,
-		ShadowLiquidity:            msg.Amount,            // temp
-		ShadowLpCoinDenom:          msg.Amount.GetDenom(), // temp
+		ShadowLiquidity:            msg.Amount,            // TODO: use shadow
+		ShadowLpCoinDenom:          msg.Amount.GetDenom(), // TODO: use shadow denom
 		ShadowTotalNormalDeposited: 0,
 		ShadowTotalConlyDeposited:  0,
 		ShadowTotalBorrowed:        0,
-		LastUpdated:                0, // temp
+		LastUpdated:                0, // TODO: current timestamp
 	}
 
 	k.AppendPairPool(ctx, pool)
