2478a1e9772ece593b84616b0136532cd8f298f0
linnefromice <linnefromice@gmail.com>
2022-11-22 16:37:56 +0900

    clean: remove unused

--- x/lending/types/types.go
+++ x/lending/types/types.go
@@ -1,11 +1,1 @@
 package types
-
-import "fmt"
-
-func GetPairPoolAssetLpCoinDenom(denom string) string {
-	return fmt.Sprintf("s%s", denom)
-}
-
-func GetPairPoolShadowLpCoinDenom(assetDenom string, shadowDenom string) string {
-	return fmt.Sprintf("s%s-%s", shadowDenom, assetDenom)
-}
